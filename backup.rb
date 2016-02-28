704bb26e0c9084cfec0857df22951d0488e90251
Aaron Van Bokhoven <bokhoven@gmail.com>
2016-02-27 22:56:34 -1000

    Add bitbar flag.

--- backup.rb
+++ backup.rb
@@ -7,7 +7,8 @@ settings = {
   log_path: "/Users/aaronvb/.tarsnap/logs",
   to_backup: "/Users/aaronvb/projects",
   backup_name: "projects",
-  prune_backups: 259200 # 3 days in seconds
+  prune_backups: 259200, # 3 days in seconds
+  bitbar: true
 }
 
 tarsnap = Tarsnap.new(settings)
