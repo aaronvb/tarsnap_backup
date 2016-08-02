00e7768d92effd5a823040cba809898690f6c5d1
Aaron Van Bokhoven <bokhoven@gmail.com>
2016-08-02 11:10:02 -1000

    Skip backup if running on battery.

--- backup.rb
+++ backup.rb
@@ -2,6 +2,11 @@
 
 require_relative 'tarsnap'
 
+# Return if running on battery
+battery_status = `pmset -g batt`
+battery_status = battery_status.split(/\r?\n/).first
+return if battery_status == "Now drawing from 'Battery Power'"
+
 settings = {
   where_tarsnap: "/usr/local/bin/tarsnap",
   log_path: "/Users/aaronvb/.tarsnap/logs",
